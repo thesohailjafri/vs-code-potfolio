3c7ae422e1107b1b70367de502b4a13b817cd245
sohail-webenetic <101564364+sohail-webenetic@users.noreply.github.com>
2024-09-03 20:07:04 +0530

    chore: Update page layout and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@
 'use client'
 import {
   Button,
+  ButtonGroup,
   chakra,
   Heading,
   HTMLChakraProps,
+  Stack,
   Text,
 } from '@chakra-ui/react'
 import { Link } from '@chakra-ui/next-js'
@@ -16,8 +18,7 @@ export default function Page() {
     <Box position={'relative'} height={'full'} width={'full'}>
       <Box
         id="background"
-        fontSize={{ base: '5rem' }}
-        lineHeight={'1.1'}
+        lineHeight={'1'}
         color={'primary.bgText'}
         position={'absolute'}
         fontWeight={'bold'}
@@ -27,8 +28,14 @@ export default function Page() {
         justifyContent={'center'}
         flexDirection={'column'}
       >
-        <Text>I BUILD</Text>
-        <Text>WEBSITES</Text>
+        <Text
+          fontSize={{ base: '5rem', md: '10rem', lg: '15rem', xl: '20rem' }}
+          fontFamily={'sans'}
+        >
+          I BUILD
+          <br />
+          WEBSITES
+        </Text>
       </Box>
 
       <Box
@@ -37,17 +44,37 @@ export default function Page() {
         zIndex={3}
         height={'full'}
         display={'flex'}
-        justifyContent={'center'}
+        justifyContent={{
+          base: 'start',
+          xl: 'center',
+        }}
+        gap={20}
         flexDirection={'column'}
       >
-        <Heading as="h1">Sohail Jafri</Heading>
-        <Heading as="h6">Full Stack Web Developer</Heading>
-        <Button as={Link} href="/projects">
-          View Work
-        </Button>
-        <Button as={Link} href="/contact">
-          Contact Me
-        </Button>
+        <Stack spacing={4}>
+          <Heading
+            as="h1"
+            fontSize={{
+              base: '3xl',
+              md: '4xl',
+              lg: '5xl',
+              xl: '8xl',
+            }}
+          >
+            Sohail Jafri
+          </Heading>
+          <Heading as="h6" fontWeight={'normal'}>
+            Full Stack Web Developer
+          </Heading>
+        </Stack>
+        <ButtonGroup>
+          <Button as={Link} href="/projects">
+            View Work
+          </Button>
+          <Button as={Link} href="/contact">
+            Contact Me
+          </Button>
+        </ButtonGroup>
       </Box>
       <HeroIllustration />
     </Box>
@@ -56,41 +83,35 @@ export default function Page() {
 
 const HeroIllustration = () => {
   return (
-    <chakra.svg
-      width={{
-        base: '75vw',
-        md: '420px',
-        lg: '600px',
-        xl: '650px',
+    <Box
+      position="absolute"
+      bottom="0"
+      right="0"
+      transform={{
+        base: 'scale(0.5)',
+        md: 'scale(0.75)',
+        lg: 'scale(1)',
+        xl: 'scale(1.25)',
       }}
-      height={{
-        base: '50vh',
-        md: '550px',
-        lg: '700px',
-        xl: '800px',
-      }}
-      position={{
-        base: 'absolute',
-      }}
-      bottom={{
-        base: '0',
-      }}
-      right={{
-        base: '0',
-      }}
-      viewBox="0 0 486 534"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+      transformOrigin="bottom right"
       zIndex={2}
     >
-      <circle cx={167} cy={60} r={60} fill="#D7F484" />
-      <circle cx={37.5} cy={215.5} r={37.5} fill="currentColor" />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M486 144.469c-38.145-31.86-87.255-51.033-140.842-51.033-121.415 0-219.842 98.427-219.842 219.842 0 14.167 1.34 28.02 3.9 41.441 47.414-86.154 91.678-142.17 146.717-170.767 56.069-29.132 121.816-29.08 210.067-6.68v-32.803zm0 48.288v289.33c-38.145 31.86-87.255 51.033-140.842 51.033-100.321 0-184.947-67.197-211.325-159.037l1.502.805c49.937-93.22 94.046-149.844 147.514-177.625 52.014-27.025 114.411-27.498 203.151-4.506z"
-        fill="currentColor"
-      />
-    </chakra.svg>
+      <chakra.svg
+        width={'486px'}
+        height={'534px'}
+        viewBox="0 0 486 534"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx={167} cy={60} r={60} fill="#D7F484" />
+        <circle cx={37.5} cy={215.5} r={37.5} fill="currentColor" />
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M486 144.469c-38.145-31.86-87.255-51.033-140.842-51.033-121.415 0-219.842 98.427-219.842 219.842 0 14.167 1.34 28.02 3.9 41.441 47.414-86.154 91.678-142.17 146.717-170.767 56.069-29.132 121.816-29.08 210.067-6.68v-32.803zm0 48.288v289.33c-38.145 31.86-87.255 51.033-140.842 51.033-100.321 0-184.947-67.197-211.325-159.037l1.502.805c49.937-93.22 94.046-149.844 147.514-177.625 52.014-27.025 114.411-27.498 203.151-4.506z"
+          fill="currentColor"
+        />
+      </chakra.svg>
+    </Box>
   )
 }
